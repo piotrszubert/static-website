996aeae6a6cbfcd9fe0d24308cf21291275199d9
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-01-17 22:24:37 +0100

    theme in local storage

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-const themeSelect = document.getElementById('themeSelect');
-
-themeSelect.addEventListener('change', event => {
-    const selectedTheme = event.currentTarget.value;
-    console.log(event);
-
-    selectedTheme == 'dark' 
-    ? document.body.classList.add('dark-mode')
-    : document.body.classList.remove('dark-mode');
-
+document.getElementById('themeToggleButton').addEventListener('click', e => {
+    // console.log(e);
+    toggleTheme(e);
 })
+
+function setTheme(theme) {
+    if(theme == 'dark') {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+}
+
+window.onload = function() {
+    const currentTheme = localStorage.getItem('currentTheme');
+    setTheme(currentTheme);
+}
+
+function toggleTheme() {
+    const currentTheme = localStorage.getItem('currentTheme');
+
+    currentTheme === 'dark' 
+    ? localStorage.setItem('currentTheme', 'dark') 
+    : localStorage.setItem('currentTheme', 'light');
+    console.log(currentTheme)
+
+    if (currentTheme == 'dark') {
+        console.log('chuj')
+        localStorage.setItem('currentTheme', 'light');
+        setTheme('light');
+    } else {
+        localStorage.setItem('currentTheme', 'dark');
+        setTheme('dark');
+    }
+
+}
 
 slideInSections = document.querySelectorAll('.slide-in-left');
