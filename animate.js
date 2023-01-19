e5b2ee1921d1ddc29d2cd3629334fe151771234f
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-01-19 21:18:18 +0100

    added drop-in effect

--- animate.js
+++ animate.js
@@ -5,8 +5,17 @@ const observer = new IntersectionObserver(entries => {
         } else {
             // entry.target.classList.remove('show')
         }
+
+        if (entry.isIntersecting && entry.target.classList.contains('drop-in-hidden')) {
+            entry.target.classList.add('drop-in-show')
+        } else {
+            // entry.target.classList.remove('drop-in-show')
+        }
     })
 })
 
 slideInSections = document.querySelectorAll('.hidden');
 slideInSections.forEach(element => observer.observe(element))
+
+dropInSections = document.querySelectorAll('.drop-in-hidden');
+dropInSections.forEach(element => observer.observe(element))
